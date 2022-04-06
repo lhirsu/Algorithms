dd17897ca9d78260970928ca904c334155c866fb
Lucian Hirsu <lucian.hirsu@adswizz.com>
2022-04-06 12:26:41 +0300

    * adding some interview problems

--- src/main/java/problems/interview/StringCollapse.java
+++ src/main/java/problems/interview/StringCollapse.java
@@ -9,7 +9,8 @@ public class StringCollapse {
 
     public static void main(String[] args) {
         StringCollapse stringCollapse = new StringCollapse();
-        String result = stringCollapse.collapseString("AAABBCCCDDDD");
+//        String result = stringCollapse.collapseString("AAABBCCCDDDD");
+        String result = stringCollapse.collapseStringForRepeatedSequences("AAABBCCCDDDD");
         System.out.println(result);
     }
 
@@ -27,6 +28,32 @@ public class StringCollapse {
                 .stream()
                 .map(e -> e.getKey() + e.getValue())
                 .reduce("", String::concat);
+    }
+
+    public String collapseStringForRepeatedSequences(String s) {
+
+        String currLetter = "";
+        int currOccur = 0;
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0) {
+                if (s.charAt(i) == s.charAt(i - 1)) {
+                    currOccur = currOccur + 1;
+                } else {
+                    result += currLetter + currOccur;
+                    // reset
+                    currLetter = String.valueOf(s.charAt(i));
+                    currOccur = 1;
+                }
+            } else {
+                currLetter = String.valueOf(s.charAt(i));
+                currOccur = 1;
+            }
+        }
+
+        result += currLetter + currOccur; // this is for the last sequence
+        return result;
     }
 
 }
