0ad2cd295572f4a94ff49e29853004fc5ff3047a
lucian.hirsu <lucian.hirsu@adswizz.com>
2025-06-10 11:21:22 +0300

    * adding problem

--- src/main/java/problems/interview/Palindrome.java
+++ src/main/java/problems/interview/Palindrome.java
@@ -1,5 +1,7 @@
 package problems.interview;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,23 +13,40 @@ public class Palindrome {
         System.out.println("ccaai could be a palindrome: " + palindrome.couldBePalindrome("ccaai"));
         System.out.println("etalate could be a palindrome: " + palindrome.couldBePalindrome("etalate"));
         System.out.println("etalat could be a palindrome: " + palindrome.couldBePalindrome("etalat"));
+
+        System.out.println("------------------------------------------------------------");
+
+        System.out.println("caiac is palindrome: " + palindrome.isPalindrome("caiac"));
+        System.out.println("ccaai is palindrome: " + palindrome.isPalindrome("ccaai"));
+        System.out.println("etalate is palindrome: " + palindrome.isPalindrome("etalate"));
+        System.out.println("etalat is palindrome: " + palindrome.isPalindrome("etalat"));
     }
 
     public boolean couldBePalindrome(String s) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            String key = String.valueOf(s.charAt(i));
-            int count = map.getOrDefault(key, 0);
-            map.put(key, count + 1);
+            Character key = s.charAt(i);
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
-        long counter = map.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() % 2 == 1)
-                .count();
+        return map.values().stream().filter(value -> value % 2 == 1).count() <= 1;
+    }
 
-        return counter <= 1;
+    public boolean isPalindrome(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            deque.add(s.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
